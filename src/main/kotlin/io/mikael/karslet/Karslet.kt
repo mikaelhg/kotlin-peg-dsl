7bb395f496757839fce327bbf51f9e0dd5e43391
Mikael Gueck <gumi@iki.fi>
2021-06-12 10:02:36 +0300

    Dox and minor refactoring.

--- src/main/kotlin/io/mikael/karslet/Karslet.kt
+++ src/main/kotlin/io/mikael/karslet/Karslet.kt
@@ -4,22 +4,24 @@ import io.mikael.karslet.operators.SequenceOperator
 import io.mikael.karslet.operators.OrderedChoiceOperator
 import io.mikael.karslet.operators.RepeatingOperator
 
+const val MAX_REPEATS = Integer.MAX_VALUE
+
+/**
+ * Karslet is a Parsing Expression Grammar (PEG) domain specific language (DSL) library.
+ */
 object Karslet {
 
     fun <T> choice(init: OrderedChoiceOperator<T>.() -> Unit) = OrderedChoiceOperator<T>().also(init)
 
     fun <T> sequence(init: SequenceOperator<T>.() -> Unit) = SequenceOperator<T>().also(init)
 
-    fun <T> oneOrMore(init: RepeatingOperator<T>.() -> Unit) =
-        repeat(min = 1, init = init)
-
-    fun <T> zeroOrMore(init: RepeatingOperator<T>.() -> Unit) =
-        repeat(min = 0, init = init)
-
-    fun <T> optional(init: RepeatingOperator<T>.() -> Unit) =
-        repeat(min = 0, max = 1, init = init)
-
-    fun <T> repeat(min: Int = 0, max: Int = Integer.MAX_VALUE, init: RepeatingOperator<T>.() -> Unit) =
+    fun <T> repeat(min: Int = 0, max: Int = MAX_REPEATS, init: RepeatingOperator<T>.() -> Unit) =
         RepeatingOperator<T>(min, max).also(init)
+
+    fun <T> optional(init: RepeatingOperator<T>.() -> Unit) = repeat(min = 0, max = 1, init = init)
+
+    fun <T> zeroOrMore(init: RepeatingOperator<T>.() -> Unit) = repeat(min = 0, init = init)
+
+    fun <T> oneOrMore(init: RepeatingOperator<T>.() -> Unit) = repeat(min = 1, init = init)
 
 }
--- src/main/kotlin/io/mikael/karslet/operators/NonTerminalOperator.kt
+++ src/main/kotlin/io/mikael/karslet/operators/NonTerminalOperator.kt
@@ -1,6 +1,7 @@
 package io.mikael.karslet.operators
 
 import io.mikael.karslet.Karslet
+import io.mikael.karslet.MAX_REPEATS
 import io.mikael.karslet.Parser
 import io.mikael.karslet.ParserConfiguration
 
@@ -8,8 +9,17 @@ abstract class NonTerminalOperator<T> : Parser<T> {
 
     val children: MutableList<Parser<*>> = mutableListOf()
 
+    /*
+     * This lambda will be called by the parse() method, before the children's parse is called.
+     * Since operators can be reused, and the previous call might have polluted the state, this
+     * is where you'd want to reset your state, and make it like freshly fallen snow.
+     */
     var beforeAttemptAction: () -> Unit = {}
 
+    /*
+     * This lambda will be called by the parent of this operator, to fetch this operator's state
+     * after a successful (or otherwise) execution.
+     */
     lateinit var successAction: () -> T
 
     @ParserConfiguration
@@ -22,35 +32,37 @@ abstract class NonTerminalOperator<T> : Parser<T> {
         this.successAction = successAction
     }
 
-    fun characters(min: Int = 1, max: Int = Integer.MAX_VALUE, matcher: (Char) -> Boolean) =
-        add(MatchCharacters()) { this.min = min; this.max = max; match(matcher) }
-
-    fun characters(min: Int = 1, max: Int = Integer.MAX_VALUE, char: Char) =
-        add(MatchCharacters()) { this.min = min; this.max = max; match { it == char } }
-
-    fun character(char: Char) =
-        add(MatchCharacters()) { min = 1; max = 1; match { it == char } }
-
-    fun character(vararg char: Char) =
-        add(MatchCharacters()) { min = 1; max = 1; match { it in char } }
+    /* Terminal operations */
 
     fun characters(init: MatchCharacters.() -> Unit) = add(MatchCharacters(), init)
 
-    fun whitespace(min: Int = 0, max: Int = Integer.MAX_VALUE) =
-        add(MatchCharacters()) { this.min = min; this.max = max; match { it.isWhitespace() } }
+    fun characters(min: Int = 1, max: Int = MAX_REPEATS, char: Char) = characters(min, max) { it == char }
+
+    fun character(char: Char) = characters(1, 1) { it == char }
+
+    fun character(vararg char: Char) = characters(1, 1) { it in char }
+
+    fun characters(min: Int = 1, max: Int = MAX_REPEATS, matcher: (Char) -> Boolean) =
+        characters(init = { this.min = min; this.max = max; match(matcher) })
+
+    fun whitespace(min: Int = 0, max: Int = MAX_REPEATS) =
+        characters(min, max) { it.isWhitespace() }
+
+    /* Non-terminal operations */
 
     fun <T> choice(init: OrderedChoiceOperator<T>.() -> Unit) = include(Karslet.choice(init))
 
     fun <T> sequence(init: SequenceOperator<T>.() -> Unit) = include(Karslet.sequence(init))
 
+    fun <T> repeat(min: Int = 1, max: Int = MAX_REPEATS, init: RepeatingOperator<T>.() -> Unit) =
+        include(Karslet.repeat(min, max, init))
+
+    fun <T> optional(init: RepeatingOperator<T>.() -> Unit) = include(Karslet.optional(init))
+
     fun <T> oneOrMore(init: RepeatingOperator<T>.() -> Unit) = include(Karslet.oneOrMore(init))
 
     fun <T> zeroOrMore(init: RepeatingOperator<T>.() -> Unit) = include(Karslet.zeroOrMore(init))
 
-    fun <T> repeat(min: Int = 1, max: Int = Integer.MAX_VALUE, init: RepeatingOperator<T>.() -> Unit) =
-        include(Karslet.repeat(min, max, init))
-
-    @SuppressWarnings("WeakerAccess")
     protected fun <T : Parser<*>> add(child: T, init: T.() -> Unit) =
         child.also(init).also(children::add)
 
